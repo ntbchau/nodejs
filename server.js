efc37df9123432c4413c2ad22bb3811d1bfafdca
ntbchau <ntbchau2301@gmail.com>
2023-04-25 21:10:03 +0200

    hello world

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const http = require('http');
+const express = require('express');
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log('run request....')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('Hello World');
-    res.end();
+app.get('/', (req, res) => {
+    res.send('Hello World from Chau');
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Nodee.js is running in port 3000');
+app.get('/about', (req, res) => {
+    res.send(`I'm Chau`)
+})
+
+
+app.listen(port, () => {
+    console.log('First app is running in port 3000');
 })
