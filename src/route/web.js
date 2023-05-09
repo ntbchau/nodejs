dbf0d8863bac3c1a868dc4c513e57e6ea9de67d1
ntbchau <ntbchau2301@gmail.com>
2023-05-09 18:47:23 +0200

    Post request

--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,7 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/details/user/:id', homeController.getDetailPage);
+    router.post('/create-new-user', homeController.createNewUser);
     router.get('/about', (req, res) => {
         res.send(`I'm Chau`)
     })
--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 configViewEngine(app);
 
 initWebRoute(app);
